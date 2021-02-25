ddf6897d7f1e81de83c5825baf1024273b76d0d1
Luis Barqueira <65919567+lbarqueira@users.noreply.github.com>
2021-02-25 15:59:19 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# baby_names
+
+[Firebase for Flutter Codelab](https://codelabs.developers.google.com/codelabs/flutter-firebase#0)
+
+Note: Based on a Codelab that it is not updated. Refer to the following documentation to update: [The official Firebase plugins for Flutter](https://firebase.flutter.dev/)
+This sample app works on Android devices and Web. 
+
+## App
+
+[![baby-names.png](https://i.postimg.cc/s2VZzfHQ/baby-names.png)](https://postimg.cc/9Dnft2hC)
---
+++ lib/main.dart
@@ -0,0 +1,165 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  // Set default `_initialized` and `_error` state to false
+  bool _initialized = false;
+  bool _error = false;
+
+  // Define an async function to initialize FlutterFire
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initializeFlutterFire();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Show error message if initialization failed
+    if (_error) {
+      return Text('Something Went Wrong');
+    }
+
+    // Show a loader until FlutterFire is initialized
+    if (!_initialized) {
+      return Center(
+          child: CircularProgressIndicator(
+        backgroundColor: Colors.black,
+      ));
+    }
+
+    return HomePage();
+  }
+}
+
+final List<Map<String, dynamic>> dummySnapshot = [
+  {"name": "Filip", "votes": 15},
+  {"name": "Abraham", "votes": 14},
+  {"name": "Richard", "votes": 11},
+  {"name": "Ike", "votes": 10},
+  {"name": "Justin", "votes": 1},
+];
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Baby Names',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() {
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Baby Name Votes'),
+      ),
+      body: _buildBody(context),
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('baby').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+        return _buildList(context, snapshot.data.docs);
+      },
+    );
+  }
+
+  Widget _buildList(
+      BuildContext context, List<QueryDocumentSnapshot> snapshot) {
+    snapshot.forEach((element) {
+      print(element.data());
+    });
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, QueryDocumentSnapshot data) {
+    final record = Record.fromSnapshot(data);
+    print(record.reference.id);
+
+    return Padding(
+      key: ValueKey(record.name),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+          title: Text(record.name),
+          trailing: Text(record.votes.toString()),
+          //onTap: () => record.reference.update({'votes': record.votes + 1}),
+          onTap: () =>
+              record.reference.update({'votes': FieldValue.increment(1)}),
+        ),
+      ),
+    );
+  }
+}
+
+class Record {
+  final String name;
+  final int votes;
+  final DocumentReference reference;
+
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        assert(map['votes'] != null),
+        name = map['name'],
+        votes = map['votes'];
+
+  //Record.fromSnapshot(QueryDocumentSnapshot snapshot)
+  //    : this.fromMap(snapshot.data(), reference: snapshot.reference);
+
+  Record.fromSnapshot(QueryDocumentSnapshot snapshot)
+      : assert(snapshot.data()['name'] != null),
+        assert(snapshot.data()['votes'] != null),
+        assert(snapshot.reference != null),
+        name = snapshot.data()['name'],
+        votes = snapshot.data()['votes'],
+        reference = snapshot.reference;
+
+  @override
+  String toString() => "Record<$name:$votes>";
+}
